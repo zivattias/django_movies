82352313b4c8729e433054168da75ab587e346ed
zivattias <ziv.attias7@gmail.com>
2023-01-29 11:05:54 +0200

    added movie_actors & Actor model to movies_dj db

--- movies_app/models.py
+++ movies_app/models.py
@@ -1,24 +1,51 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+class Actor(models.Model):
+
+    name = models.CharField(max_length=256, db_column='name', null=False, blank=False)
+    birth_year = models.IntegerField(db_column='birth_year', null=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'actors'
 
 class Movie(models.Model):
     movie_name = models.CharField(db_column='movie_name', max_length=256, null=False)
     description = models.TextField(db_column='description', null=False)
     duration = models.FloatField(db_column='duration', null=False)
-    release_year = models.IntegerField(db_column='year', null=False,
+    release_year = models.IntegerField(db_column='release_year', null=False,
                                        validators=[MinValueValidator(1900), MaxValueValidator(2050)])
     pic_url = models.URLField(db_column='pic_url', max_length=512, null=True)
+    actors = models.ManyToManyField(to=Actor, through='MovieActor')
+
+    def __str__(self):
+        return self.movie_name
 
     class Meta:
         db_table = 'movies'
+
+class MovieActor(models.Model):
+    actor = models.ForeignKey(Actor, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    salary = models.IntegerField()
+    main_role = models.BooleanField(null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.actor.name} in movie {self.movie.name}"
+
+
+    class Meta:
+        db_table = 'movie_actors'
 
 
 class Rating(models.Model):
     movie = models.ForeignKey("Movie", on_delete=models.RESTRICT)
     rating = models.SmallIntegerField(db_column='rating', null=False, validators=[MinValueValidator(0),
                                                                                   MaxValueValidator(11)])
-    rating_date = models.DateField(db_column='date', null=False)
+    rating_date = models.DateField(db_column='rating_date', null=False)
 
     class Meta:
         db_table = 'ratings'
@@ -26,8 +53,8 @@ class Rating(models.Model):
 
 class Review(models.Model):
     movie = models.ForeignKey("Movie", on_delete=models.RESTRICT)
-    review_text = models.TextField(db_column='text', null=False)
-    review_date = models.DateField(db_column='date', null=False)
+    review_text = models.TextField(db_column='review_text', null=False)
+    review_date = models.DateField(db_column='review_date', null=False)
 
     class Meta:
         db_table = 'reviews'
