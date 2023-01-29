50f8d0b2a5a0d59a96074965d0b91caa73dd19ea
zivattias <ziv.attias7@gmail.com>
2023-01-29 10:49:11 +0200

    added tests

--- my_test.py
+++ my_test.py
@@ -25,5 +25,30 @@ from movies_app.models import Movie, Rating
 # for movie in Movie.objects.all():
 #     Rating(movie=movie, rating=random.randint(1, 10)).save()
 
-m = Movie.objects.get(pk=3)
-print(m.rating_set.all())
+# m = Movie.objects.get(pk=3)
+# print(m.rating)
+
+# r = Rating.objects.get(pk=1)
+# all_r = Rating.objects.all()[:500]
+# print(r.movie_id)
+# print(all_r)
+
+# m = Movie.objects.get(pk=1)
+# for rating in m.rating_set.all():
+#     print(rating.rating)
+
+# Executes an SQL query on the spot, expecting 1 result ONLY
+# Movie.objects.get(movie_name='b')
+
+# Executes an SQL query ONLY when data is accessed/iterated through Python code
+# Movie.objects.all()
+# Executes now:
+# movies = Movie.objects.all().values_list('movie_name', 'duration')
+# print(movies[2])
+
+movies_qs = Movie.objects.all().values_list('movie_name', 'duration')
+print(movies_qs.query)
+movies_qs = movies_qs.filter(release_year__gt=2020)
+print(movies_qs.query)
+movies_qs = movies_qs[:2]
+print(movies_qs.query)
