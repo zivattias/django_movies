895b51d64b48fa03c409ecdb757193f394f0c606
zivattias <ziv.attias7@gmail.com>
2023-02-05 14:44:34 +0200

    my first JS code

--- web_front_practice/static/scripts/my_script.js
+++ web_front_practice/static/scripts/my_script.js
@@ -4,3 +4,24 @@ function showAlert() {
     window.alert("Loaded")
 }
 
+function changeHeaderToRed() {
+    elem = document.getElementById('my-header')
+    elem.style.cssText = 'color: red !important'
+}
+
+function switchColor(className) {
+    const elems = document.getElementsByClassName(className)
+    for (let index = 0; index < elems.length; index++) {
+        const elem = elems[index];
+        let currClass = null;
+        let newClass = null;
+        if (elem.classList.contains('btn-primary')) {
+            currClass = 'btn-primary'
+            newClass = 'btn-danger'
+        } else {
+            currClass = 'btn-danger'
+            newClass = 'btn-primary'
+        }
+        elem.classList.replace(currClass, newClass);
+    }
+}
